4ec511d3f5da3ecb8adad6ea1195fcf9958881cb
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-10-17 09:40:42 +0000

    [Add]userのupdate機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update
+    @user.update(user_params)
     redirect_to user_path(@user.id)
   end
 
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :Introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
